564cecf3c9ee87358ce6f0ca84a8f506d695bcfe
Geoffrey Goodman <ggoodman@gmail.com>
2016-04-05 15:33:46 -0400

    Add example of loading package.json

--- example/lib/index.js
+++ example/lib/index.js
@@ -1,5 +1,6 @@
 var Express = require('express');
 var Joi = require('joi'); // I know that this is not on sandbox. Let webpack bundle it for webtask usage.
+var Package = require('../package.json'); // Test of loading json
 
 
 var app = Express();
